6f713840a9dc02dc555b6f0b728fb297fad0089f
pinkyo <pinkyo@outlook.com>
2017-10-06 23:06:56 +0800

    Others#, add jetty thread pool configuration.

--- src/main/java/my/pinkyo/demo/DemoConfiguration.java
+++ src/main/java/my/pinkyo/demo/DemoConfiguration.java
@@ -1,16 +1,18 @@
 package my.pinkyo.demo;
 
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.eclipse.jetty.util.thread.ExecutorThreadPool;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @MapperScan(value = "my.pinkyo.demo.mapper")
@@ -28,4 +30,14 @@ public class DemoConfiguration {
         sessionFactory.setDataSource(dataSource);
         return sessionFactory.getObject();
     }
+
+    @Bean
+    JettyEmbeddedServletContainerFactory factory() {
+        JettyEmbeddedServletContainerFactory factory =
+                new JettyEmbeddedServletContainerFactory();
+        factory.setThreadPool(new ExecutorThreadPool(
+                50, 200, 15, TimeUnit.MINUTES));
+        return factory;
+    }
+
 }
