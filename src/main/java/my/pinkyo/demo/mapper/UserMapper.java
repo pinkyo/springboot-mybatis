5a76b26db9cbaca3499de15564241f489f09ee14
pinkyo <yinkn@outlook.com>
2018-06-01 20:01:55 +0800

    Others#, fix.

--- src/main/java/my/pinkyo/demo/mapper/UserMapper.java
+++ src/main/java/my/pinkyo/demo/mapper/UserMapper.java
@@ -5,13 +5,18 @@ import my.pinkyo.demo.typehandler.UUIDTypeHandler;
 import org.apache.ibatis.annotations.*;
 
 import my.pinkyo.demo.model.User;
+import org.apache.ibatis.type.EnumTypeHandler;
 
 import java.util.List;
 import java.util.UUID;
 
 @Mapper
 public interface UserMapper {
-    @ResultType(User.class)
+    @Results(id = "getUserByName", value = {
+            @Result(id = true, property = "id", column = "id", typeHandler = UUIDTypeHandler.class),
+            @Result(property = "name", column = "name"),
+            @Result(property = "sex", column = "sex", typeHandler = EnumTypeHandler.class)
+    })
 	@SelectProvider(type = UserSqlProvider.class, method = "getUserByName")
 	User getUserByName(@Param("name") String name);
 
