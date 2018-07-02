cc5fc07cd90aa7035eb65cc9c009a884ac307849
pinkyo <yinkn@outlook.com>
2018-07-02 22:35:31 +0800

    Others#, fix.

--- src/main/java/my/pinkyo/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/my/pinkyo/demo/exception/GlobalExceptionHandler.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
