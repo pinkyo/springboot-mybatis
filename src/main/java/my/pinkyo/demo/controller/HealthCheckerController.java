8ca67f66393402483de601d0a851add128bc1c4c
pinkyo <yinkn@outlook.com>
2018-07-05 23:29:29 +0800

    Others#, fix.

---
+++ src/main/java/my/pinkyo/demo/controller/HealthCheckerController.java
@@ -0,0 +1,30 @@
+package my.pinkyo.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.DataSourceHealthIndicator;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+@RestController
+public class HealthCheckerController {
+
+    @Autowired
+    private DataSource dataSource;
+    private DataSourceHealthIndicator dshl;
+
+    @PostConstruct
+    public void setup() {
+        dshl = new DataSourceHealthIndicator(dataSource);
+    }
+
+    @RequestMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    public String health() {
+        return dshl.health().getStatus().getDescription();
+    }
+}
